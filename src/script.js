f9b47a6a951977358b8e0590b324ae1422f411ec
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2022-03-02 20:09:14 +0000

    Created functions for collions, added win and lose pages, included reset button

--- src/script.js
+++ src/script.js
@@ -1,24 +1,28 @@
-
-let gameover=false;
+let gameIsOver = false;
+let score = 0;
 
 let gameIntro = document.querySelector(".game-intro");
 let gamePlay = document.querySelector("#game-play");
-let gameOver = document.querySelector(".game-over");
+let gameOverWin = document.querySelector(".win");
+let gameOverLose = document.querySelector(".lose");
 
 let screenWidth;
-
 
 //// buttons ////
 
 let startBtn = document.querySelector("#start-button");
-
+let restartBtn = document.querySelector("#restart-button");
 
 //// character variables ////
 
-let ufoX=525;
-let ufoY=300;
-let ufoHeight= 300
-let ufoWidth = 300
+
+let ufoHeight = 225;
+let ufoWidth = 225;
+
+let asteroidHeight = 150;
+let asteroidWidth = 150;
+let characterHeight = 150;
+let characterWidth = 150;
 
 //// object variables ////
 
@@ -33,126 +37,182 @@ let snuffles;
 let mrPB;
 let lincoler;
 
-let asteroidArr = [ 
-    {x: Math.floor(Math.random()* 1000 ), y: -200},
-    {x: Math.floor(Math.random()* 1000 ), y: -400},
-    {x: Math.floor(Math.random()* 1000 ), y: -600},
-    {x: Math.floor(Math.random()* 1000 ), y: -800},
-    {x: Math.floor(Math.random()* 1000 ), y: -900},
-    {x: Math.floor(Math.random()* 1000 ), y: -1100},
-    {x: Math.floor(Math.random()* 1000 ), y: -1200},
-    {x: Math.floor(Math.random()* 1000 ), y: -1300},
-
+let asteroidArr = [
+  { x: Math.floor(Math.random() * 1000), y: -200 },
+  // { x: Math.floor(Math.random() * 1000), y: -400 },
+  // { x: Math.floor(Math.random() * 1000), y: -600 },
+  // { x: Math.floor(Math.random() * 1000), y: -800 },
+  // { x: Math.floor(Math.random() * 1000), y: -1000 },
+  // { x: Math.floor(Math.random() * 1000), y: -1200 },
+  // { x: Math.floor(Math.random() * 1000), y: -1400 },
+  // { x: Math.floor(Math.random() * 1000), y: -1600 },
 ];
 
-let charactersArr = [ ]
-
-
-
-
-
+let charactersArr = [];
 
 function preload() {
-    bg = loadImage('../images/Space-Background.jpg');
-    ufo = loadImage('../images/UFO.png');
-    asteroid = loadImage('../images/asteroid1.png');
-    rick = loadImage('../images/Rick.png');
-    morty = loadImage('../images/Morty.png');
-    jerry = loadImage('../images/Jerry.png');
-    squanchy = loadImage('../images/squanchy.png');
-    meeseeks = loadImage('../images/Mr. Meeseeks.png');
-    picklerick = loadImage('../images/Pickle-Rick.png');
-    snuffles = loadImage('../images/Snuffles.png');
-    mrPB = loadImage('../images/Mr PB.png');
-    lincoler = loadImage('../images/Abrodolph Lincoler.png');
+  bg = loadImage("../images/Space-Background.jpg");
+  ufo = loadImage("../images/UFO.png");
+  asteroid = loadImage("../images/asteroid1.png");
+  rick = loadImage("../images/Rick.png");
+  morty = loadImage("../images/Morty.png");
+  jerry = loadImage("../images/Jerry.png");
+  squanchy = loadImage("../images/squanchy.png");
+  meeseeks = loadImage("../images/Mr. Meeseeks.png");
+  picklerick = loadImage("../images/Pickle-Rick.png");
+  snuffles = loadImage("../images/Snuffles.png");
+  mrPB = loadImage("../images/Mr PB.png");
+  lincoler = loadImage("../images/Abrodolph Lincoler.png");
 
-    charactersArr = [
-        {x: Math.floor(Math.random()* 1000 ), y: -2000, img:rick},
-        {x: Math.floor(Math.random()* 1000 ), y: -4000, img:morty},
-        {x: Math.floor(Math.random()* 1000 ), y: -6000, img:jerry},
-        {x: Math.floor(Math.random()* 1000 ), y: -7000, img:squanchy},
-        {x: Math.floor(Math.random()* 1000 ), y: -6500, img:meeseeks},
-        {x: Math.floor(Math.random()* 1000 ), y: -8000, img:picklerick},
-        {x: Math.floor(Math.random()* 1000 ), y: -3000, img:snuffles},
-        {x: Math.floor(Math.random()* 1000 ), y: -2750, img:mrPB},
-        {x: Math.floor(Math.random()* 1000 ), y: -9000, img:lincoler},
-    
-     ];
-
-
+  charactersArr = [
+    { x: Math.floor(Math.random() * 1000), y: -2000, img: rick },
+    // { x: Math.floor(Math.random() * 1000), y: -4000, img: morty },
+    // { x: Math.floor(Math.random() * 1000), y: -6000, img: jerry },
+    // { x: Math.floor(Math.random() * 1000), y: -7000, img: squanchy },
+    // { x: Math.floor(Math.random() * 1000), y: -6500, img: meeseeks },
+    // { x: Math.floor(Math.random() * 1000), y: -8000, img: picklerick },
+    // { x: Math.floor(Math.random() * 1000), y: -3000, img: snuffles },
+    // { x: Math.floor(Math.random() * 1000), y: -2750, img: mrPB },
+    // { x: Math.floor(Math.random() * 1000), y: -9000, img: lincoler },
+  ];
 }
-
-
 
 function setup() {
-    
-    let canvas = createCanvas(windowWidth, windowHeight);
-    canvas.parent("game-play");  
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("game-play");
+}
 
-    console.log (asteroidArr)
-
-  }
-  
 function draw() {
-    background(bg); 
-    image (ufo, ufoX, ufoY, ufoWidth, ufoHeight);
+  background(bg);
+  textSize(75);
+  textStyle(BOLDITALIC);
+  text("Score: " + score, 40, 50, fill(225));
+  image(
+    ufo,
+    mouseX - ufoWidth / 2,
+    mouseY - ufoHeight / 2,
+    ufoWidth,
+    ufoHeight
+  );
 
+  for (let i = 0; i < asteroidArr.length; i++) {
+    let currentAsteroid = asteroidArr[i];
+    image(
+      asteroid,
+      currentAsteroid.x,
+      currentAsteroid.y,
+      asteroidWidth,
+      asteroidHeight
+    );
+    currentAsteroid.y += 5;
 
-    for (let i =0; i < asteroidArr.length; i++) {
-        let currentAsteroid = asteroidArr[i]
-        image (asteroid, currentAsteroid.x, currentAsteroid.y, 250, 250)
-        currentAsteroid.y += 10
-    if ( currentAsteroid.y > windowHeight) {
-        currentAsteroid.y  = -200
-    }    
+    if (
+      currentAsteroid.y <= mouseY + ufoHeight &&
+      currentAsteroid.y + asteroidHeight >= mouseY &&
+      currentAsteroid.x + asteroidWidth - 10 >= mouseX &&
+      currentAsteroid.x + 150 <= mouseX + ufoWidth
+    ) {
+      endGameLose();
     }
 
-    for (let i =0; i < charactersArr.length; i++) {
-        let currentCharacter = charactersArr[i]
-        console.log(currentCharacter);
-        image (currentCharacter.img, currentCharacter.x, currentCharacter.y, 150, 150)
-        currentCharacter.y += 7
-    if ( currentCharacter.y > windowHeight) {
-        currentCharacter.y  = -200
+    if (currentAsteroid.y > windowHeight) {
+      currentAsteroid.y = -200;
     }
-}
-
-    
-
-
-
-
-
-}
-
-function keyPressed() {
-    if (keyCode === LEFT_ARROW){
-      ufoX += (-15)
-    } else if (keyCode === RIGHT_ARROW){
-      ufoX += (15)
-      } else if (keyCode === UP_ARROW){
-        ufoY += (-15)
-      } else if (keyCode === DOWN_ARROW){
-        ufoY += (15)
-        }
-    }
-
-function gameButtons() {
-    gameIntro.style.display = "none";
-    gamePlay.style.display = "none";
-    gameOver.style.display = "flex";
-    noLoop();
   }
 
-  window.addEventListener("load", () => {
-    gamePlay.style.display = "none";
-    gameOver.style.display = "none";
-    
+  for (let i = 0; i < charactersArr.length; i++) {
+    let currentCharacter = charactersArr[i];
 
-    startBtn.addEventListener("click", () => {
-      gameIntro.style.display = "none";
-      gamePlay.style.display = "flex";
-      gameOver.style.display = "none";
-    });
+    image(
+      currentCharacter.img,
+      currentCharacter.x,
+      currentCharacter.y,
+      characterWidth,
+      characterHeight
+    );
+    currentCharacter.y += 5;
+
+    if (
+      currentCharacter.y <= mouseY + ufoHeight &&
+      currentCharacter.y + characterHeight >= mouseY &&
+      currentCharacter.x + characterWidth - 50 >= mouseX &&
+      currentCharacter.x + 50 <= mouseX + ufoWidth
+    ) {
+      score++;
+      currentCharacter.y = -200;
+      if (score === 5 ){
+        endGameWin();
+      }
+    }
+
+    if (currentCharacter.y > windowHeight) {
+      currentCharacter.y = -200;
+    }
+  }
+}
+
+function endGameLose() {
+  gameIntro.style.display = "none";
+  gamePlay.style.display = "none";
+  gameOverLose.style.display = "flex";
+  gameOverWin.style.display = "none";
+  noLoop();
+}
+
+function endGameWin() {
+  gameIntro.style.display = "none";
+  gamePlay.style.display = "none";
+  gameOverWin.style.display = "flex";
+  gameOverLose.style.display = "none";
+  noLoop();
+}
+
+window.addEventListener("load", () => {
+  gamePlay.style.display = "none";
+  gameOverWin.style.display = "none";
+  gameOverLose.style.display = "none";
+  noLoop();
 });
 
+startBtn.addEventListener("click", () => {
+  gameIntro.style.display = "none";
+  gamePlay.style.display = "flex";
+  gameOverWin.style.display = "none";
+  gameOverLose.style.display = "none";
+  loop();
+});
+
+restartBtn.addEventListener("click", () => {
+  gameIntro.style.display = "none";
+  gamePlay.style.display = "flex";
+  gameOverWin.style.display = "none";
+  gameOverLose.style.display = "none";
+
+  gameIsOver = false;
+  score = 0;
+  asteroidArr = [
+    { x: Math.floor(Math.random() * 1000), y: -200 },
+    // { x: Math.floor(Math.random() * 1000), y: -400 },
+    // { x: Math.floor(Math.random() * 1000), y: -600 },
+    // { x: Math.floor(Math.random() * 1000), y: -800 },
+    // { x: Math.floor(Math.random() * 1000), y: -900 },
+    // { x: Math.floor(Math.random() * 1000), y: -1100 },
+    // { x: Math.floor(Math.random() * 1000), y: -1200 },
+    // { x: Math.floor(Math.random() * 1000), y: -1300 },
+  ];
+  charactersArr = [
+    { x: Math.floor(Math.random() * 1000), y: -2000, img: rick },
+    // { x: Math.floor(Math.random() * 1000), y: -4000, img: morty },
+    // { x: Math.floor(Math.random() * 1000), y: -6000, img: jerry },
+    // { x: Math.floor(Math.random() * 1000), y: -7000, img: squanchy },
+    // { x: Math.floor(Math.random() * 1000), y: -6500, img: meeseeks },
+    // { x: Math.floor(Math.random() * 1000), y: -8000, img: picklerick },
+    // { x: Math.floor(Math.random() * 1000), y: -3000, img: snuffles },
+    // { x: Math.floor(Math.random() * 1000), y: -2750, img: mrPB },
+    // { x: Math.floor(Math.random() * 1000), y: -9000, img: lincoler },
+  ];
+
+  ufo = mouseX , mouseY
+
+  loop();
+});
